b5fe137348b9162fe35e8863d2c185fbfe1ff991
AlreadyKosh <lenusui123@gmail.com>
2021-08-25 19:38:00 -0300

    teste

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,11 +3,11 @@ const produtosController = require ('../controllers/produtosController.js');
 const categoriasController = require ('../controllers/categoriasController.js')
 const router = express.Router();
  
-router.post('/produtos', categoriasController.Insert);
-router.get('/produtos', categoriasController.SelectAll);
-router.get('/produtos/:id', categoriasController.SelectDetail)
-router.put('/produtos/:id', categoriasController.Update);
-router.delete('/produtos/:id', categoriasController.Delete);
+router.post('/categorias', categoriasController.Insert);
+router.get('/categorias', categoriasController.SelectAll);
+router.get('/categorias/:id', categoriasController.SelectDetail)
+router.put('/categorias/:id', categoriasController.Update);
+router.delete('/categorias/:id', categoriasController.Delete);
 
 router.post('/produtos', produtosController.Insert);
 router.get('/produtos', produtosController.SelectAll);
